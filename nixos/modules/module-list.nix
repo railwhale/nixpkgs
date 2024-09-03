145ded66b014600ec490bacb0ae770d8f033bdf8
Niko Cantero <97130632+nyabinary@users.noreply.github.com>
2024-09-03 08:57:42 -0400

    nixos/portmaster: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1221,6 +1221,7 @@
   ./services/networking/pixiecore.nix
   ./services/networking/pleroma.nix
   ./services/networking/polipo.nix
+  ./services/networking/portmaster.nix
   ./services/networking/powerdns.nix
   ./services/networking/pppd.nix
   ./services/networking/pptpd.nix
