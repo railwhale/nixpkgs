a0879852db3363d3016381e3ec041d66720078e7
railwhale <63928521+railwhale@users.noreply.github.com>
2024-09-24 13:17:28 +1200

    nixos/portmaster: apply feedback from PR (#264454)

--- nixos/modules/services/networking/portmaster.nix
+++ nixos/modules/services/networking/portmaster.nix
@@ -17,46 +17,36 @@ let
 in
 {
   options.services.portmaster = {
-    enable = mkEnableOption "Portmaster Core Daemon";
+    enable = mkEnableOption "the Portmaster Core Daemon";
 
-    devmode.enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = ''
-        This option enables the Portmaster Core devmode.
+    devmode.enable = mkEnableOption ''
+      the Portmaster Core devmode.
 
-        The devmode makes the Portmaster UI available at `127.0.0.1:817`.
-      '';
-    };
+      The devmode makes the Portmaster UI available at `127.0.0.1:817`.
+    '';
 
     user = mkOption {
       type = types.str;
       default = "portmaster";
-      description = ''
-        The user to run Portmaster as.
-      '';
+      description = "The user to run Portmaster as.";
     };
 
     group = mkOption {
       type = types.str;
       default = "portmaster";
-      description = ''
-        The group to run Portmaster under.
-      '';
+      description = "The group to run Portmaster under.";
     };
 
     dataDir = mkOption {
       type = types.path;
       default = "/var/lib/portmaster";
-      description = ''
-        The directory where Portmaster stores its data files.
-      '';
+      description = "The directory where Portmaster stores its data files.";
     };
   };
 
   config = mkIf cfg.enable {
     users.users.${cfg.user} = {
-      group = cfg.group;
+      inherit (cfg) group;
       home = cfg.dataDir;
       createHome = true;
       isSystemUser = true;
@@ -70,27 +60,39 @@ in
       description = "Portmaster by Safing";
       documentation = [ "https://docs.safing.io" ];
 
-      after = [ "network.target" ];
+      before = [
+        "nss-lookup.target"
+        "network.target"
+        "shutdown.target"
+      ];
+      after = [ "systemd-networkd.service" ];
+      wants = [ "nss-lookup.target" ];
       wantedBy = [ "multi-user.target" ];
+
+      conflicts = [
+        "shutdown.target"
+        "firewalld.service"
+      ];
 
       path = [ pkgs.iptables ];
 
-      preStart = "${pkgs.portmaster}/bin/portmaster-start" + " --data \"${cfg.dataDir}\"" + " update";
+      preStart = "${lib.getExe' pkgs.portmaster "portmaster-start"} --data \"${cfg.dataDir}\" clean-structure ";
 
       script =
         lib.getExe' pkgs.portmaster "portmaster-core"
         + " --data \"${cfg.dataDir}\""
         + optionalString cfg.devmode.enable " -devmode";
 
-      postStop =
-        lib.getExe' pkgs.portmaster "portmaster-start" + " --data \"${cfg.dataDir}\"" + " recover-iptables";
+      postStop = "${lib.getExe' pkgs.portmaster "portmaster-start"} --data \"${cfg.dataDir}\" recover-iptables";
 
       serviceConfig = {
         Type = "simple";
         Restart = "on-failure";
-        RestartSec = "10";
+        RestartSec = 10;
+        RestartPreventExitStatus = 24;
         LockPersonality = "yes";
         MemoryDenyWriteExecute = "yes";
+        MemoryLow = "2G";
         NoNewPrivileges = "yes";
         User = cfg.user;
         Group = cfg.group;
@@ -99,17 +101,49 @@ in
         Environment = [ "LOGLEVEL=info" ];
         ProtectSystem = "true";
         ReadWritePaths = [ cfg.dataDir ];
-        RestrictAddressFamilies = "AF_UNIX AF_NETLINK AF_INET AF_INET6";
+        RestrictAddressFamilies = [
+          "AF_UNIX"
+          "AF_NETLINK"
+          "AF_INET"
+          "AF_INET6"
+        ];
         RestrictNamespaces = "yes";
         ProtectHome = "read-only";
         ProtectKernelTunables = "yes";
         ProtectKernelLogs = "yes";
         ProtectControlGroups = "yes";
         PrivateDevices = "yes";
-        AmbientCapabilities = "cap_chown cap_kill cap_net_admin cap_net_bind_service cap_net_broadcast cap_net_raw cap_sys_module cap_sys_ptrace cap_dac_override cap_fowner cap_fsetid";
-        CapabilityBoundingSet = "cap_chown cap_kill cap_net_admin cap_net_bind_service cap_net_broadcast cap_net_raw cap_sys_module cap_sys_ptrace cap_dac_override cap_fowner cap_fsetid";
-        SystemCallArchitectures = "native";
-        SystemCallFilter = "@system-service @module";
+        AmbientCapabilities = [
+          "cap_chown"
+          "cap_kill"
+          "cap_net_admin"
+          "cap_net_bind_service"
+          "cap_net_broadcast"
+          "cap_net_raw"
+          "cap_sys_module"
+          "cap_sys_ptrace"
+          "cap_dac_override"
+          "cap_fowner"
+          "cap_fsetid"
+        ];
+        CapabilityBoundingSet = [
+          "cap_chown"
+          "cap_kill"
+          "cap_net_admin"
+          "cap_net_bind_service"
+          "cap_net_broadcast"
+          "cap_net_raw"
+          "cap_sys_module"
+          "cap_sys_ptrace"
+          "cap_dac_override"
+          "cap_fowner"
+          "cap_fsetid"
+        ];
+        SystemCallArchitectures = [ "native" ];
+        SystemCallFilter = [
+          "@system-service"
+          "@module"
+        ];
         SystemCallErrorNumber = "EPERM";
       };
     };
@@ -118,5 +152,6 @@ in
   meta.maintainers = with maintainers; [
     nyanbinary
     sntx
+    railwhale
   ];
 }
