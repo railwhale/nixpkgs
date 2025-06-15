7635836a5ac6131114b0280d669bd4195a99077b
railwhale <63928521+railwhale@users.noreply.github.com>
2025-06-16 09:22:51 +1200

    temp: remove polipo module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1297,7 +1297,6 @@
   ./services/networking/pihole-ftl.nix
   ./services/networking/pixiecore.nix
   ./services/networking/pleroma.nix
-  ./services/networking/polipo.nix
   ./services/networking/portmaster.nix
   ./services/networking/powerdns.nix
   ./services/networking/pppd.nix
